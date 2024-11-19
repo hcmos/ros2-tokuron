d3acb63344601745c1aabdec43a1623fa8f1471e
hcmos <c1000702@planet.kanazawa-it.ac.jp>
2024-11-19 17:43:20 +0900

    🐛fix:初期位置の修正

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -15,17 +15,15 @@ int main(int argc, char * argv[]){
     nodes_option.automatically_declare_parameters_from_overrides(true);
 
     auto socketcan_node = std::make_shared<socketcan_interface::SocketcanInterface>(nodes_option);
-    // auto socketcan_cybergear_node = std::make_shared<socketcan_interface::SocketcanInterface>("cybergear", nodes_option);
     auto chassis_driver_node = std::make_shared<chassis_driver::ChassisDriver>(nodes_option);
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto cybergear_interface_node = std::make_shared<cybergear_interface::CybergearInterface>(nodes_option);
     auto landmark_localization_node = std::make_shared<landmark_localization::LandmarkLocalization>(nodes_option);
 
-    // exec.add_node(socketcan_node);
-    // // exec.add_node(socketcan_cybergear_node);
-    // exec.add_node(controller_node);
-    // exec.add_node(chassis_driver_node);
-    // exec.add_node(cybergear_interface_node);
+    exec.add_node(socketcan_node);
+    exec.add_node(controller_node);
+    exec.add_node(chassis_driver_node);
+    exec.add_node(cybergear_interface_node);
     exec.add_node(landmark_localization_node);
 
     exec.spin();
