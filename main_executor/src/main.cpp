9d4947ce154f6f7d87f47eb40181d40cd4768d82
RyoFunai <s24s1040du@s.chibakoudai.jp>
2024-11-29 14:17:53 +0900

    ball_detectorを追加

--- main_executor/src/main.cpp
+++ main_executor/src/main.cpp
@@ -5,6 +5,7 @@
 #include "socketcan_interface/socketcan_interface_node.hpp"
 #include "cybergear_interface/cybergear_interface_node.hpp"
 #include "landmark_localization/landmark_localization.hpp"
+#include "ball_detector/ball_detector.hpp"
 
 int main(int argc, char * argv[]){
     rclcpp::init(argc,argv);
@@ -20,13 +21,14 @@ int main(int argc, char * argv[]){
     auto controller_node = std::make_shared<controller::Controller>(nodes_option);
     auto cybergear_interface_node = std::make_shared<cybergear_interface::CybergearInterface>(nodes_option);
     auto landmark_localization_node = std::make_shared<landmark_localization::LandmarkLocalization>(nodes_option);
-
+    auto ball_detector_node = std::make_shared<ball_detector::BallDetector>(nodes_option);
     // exec.add_node(socketcan_node);
     // // exec.add_node(socketcan_cybergear_node);
     // exec.add_node(controller_node);
     // exec.add_node(chassis_driver_node);
     // exec.add_node(cybergear_interface_node);
     exec.add_node(landmark_localization_node);
+    exec.add_node(ball_detector_node);
 
     exec.spin();
     rclcpp::shutdown();
