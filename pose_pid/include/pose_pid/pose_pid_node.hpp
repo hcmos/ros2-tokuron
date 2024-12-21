89da65b61b3a7c366721a711373f441617e366b8
RyoFunai <s24s1040du@s.chibakoudai.jp>
2024-12-21 20:25:33 +0900

    targetの型を変更

--- pose_pid/include/pose_pid/pose_pid_node.hpp
+++ pose_pid/include/pose_pid/pose_pid_node.hpp
@@ -6,6 +6,7 @@
 #include <geometry_msgs/msg/twist.hpp>
 #include <geometry_msgs/msg/pose2_d.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
 
 #include "utilities/position_pid.hpp"
 
@@ -24,14 +25,15 @@ private:
     rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr _subscription_stop;
     rclcpp::Subscription<std_msgs::msg::Empty>::SharedPtr _subscription_restart;
     rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr _subscription_autonomous;
-    rclcpp::Subscription<geometry_msgs::msg::Pose2D>::SharedPtr _subscription_target;
+    rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr _subscription_target;
+    // rclcpp::Subscription<geometry_msgs::msg::Pose2D>::SharedPtr _subscription_target;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr _subscription_selfpose;
     rclcpp::TimerBase::SharedPtr _pub_timer;
 
     void _subscriber_callback_stop(const std_msgs::msg::Empty::SharedPtr msg);
     void _subscriber_callback_restart(const std_msgs::msg::Empty::SharedPtr msg);
     void _subscriber_callback_autonomous(const std_msgs::msg::Bool::SharedPtr msg);
-    void _subscriber_callback_target(const geometry_msgs::msg::Pose2D::SharedPtr msg);
+    void _subscriber_callback_target(const geometry_msgs::msg::PointStamped::SharedPtr msg);
     void _subscriber_callback_selfpose(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
     void _publisher_callback();
 
@@ -53,7 +55,7 @@ private:
 
     //変数
     bool is_autonomous = false;
-    std::shared_ptr<geometry_msgs::msg::Pose2D> target;
+    std::shared_ptr<geometry_msgs::msg::PointStamped> target;
     std::shared_ptr<geometry_msgs::msg::PoseStamped> self_pose;
 
     // 動作モード
--- pose_pid/src/pose_pid_node.cpp
+++ pose_pid/src/pose_pid_node.cpp
@@ -5,45 +5,45 @@
 
 using namespace utils;
 
-namespace pose_pid{
-
-PosePID::PosePID(const rclcpp::NodeOptions& options) : PosePID("", options) {}
-
-PosePID::PosePID(const std::string& name_space, const rclcpp::NodeOptions& options)
-: rclcpp::Node("pose_pid_node", name_space, options),
-interval_ms(get_parameter("interval_ms").as_int()),
-pid_linear_x(get_parameter("interval_ms").as_int()),
-pid_linear_y(get_parameter("interval_ms").as_int()),
-pid_angular(get_parameter("interval_ms").as_int()),
-linear_max_vel(get_parameter("linear_max.vel").as_double()),
-angular_max_vel(dtor(get_parameter("angular_max.vel").as_double())),
-allowed_area(get_parameter("allowed_area").as_double_array())
+namespace pose_pid
 {
+
+  PosePID::PosePID(const rclcpp::NodeOptions &options) : PosePID("", options) {}
+
+  PosePID::PosePID(const std::string &name_space, const rclcpp::NodeOptions &options)
+      : rclcpp::Node("pose_pid_node", name_space, options),
+        interval_ms(get_parameter("interval_ms").as_int()),
+        pid_linear_x(get_parameter("interval_ms").as_int()),
+        pid_linear_y(get_parameter("interval_ms").as_int()),
+        pid_angular(get_parameter("interval_ms").as_int()),
+        linear_max_vel(get_parameter("linear_max.vel").as_double()),
+        angular_max_vel(dtor(get_parameter("angular_max.vel").as_double())),
+        allowed_area(get_parameter("allowed_area").as_double_array())
+  {
     _subscription_stop = this->create_subscription<std_msgs::msg::Empty>(
         "stop",
         _qos,
-        std::bind(&PosePID::_subscriber_callback_stop, this, std::placeholders::_1)
-    );
+        std::bind(&PosePID::_subscriber_callback_stop, this, std::placeholders::_1));
     _subscription_restart = this->create_subscription<std_msgs::msg::Empty>(
         "restart",
         _qos,
-        std::bind(&PosePID::_subscriber_callback_restart, this, std::placeholders::_1)
-    );
+        std::bind(&PosePID::_subscriber_callback_restart, this, std::placeholders::_1));
     _subscription_autonomous = this->create_subscription<std_msgs::msg::Bool>(
         "autonomous",
         _qos,
-        std::bind(&PosePID::_subscriber_callback_autonomous, this, std::placeholders::_1)
-    );
-    _subscription_target = this->create_subscription<geometry_msgs::msg::Pose2D>(
+        std::bind(&PosePID::_subscriber_callback_autonomous, this, std::placeholders::_1));
+    _subscription_target = this->create_subscription<geometry_msgs::msg::PointStamped>(
         "target_pose",
         _qos,
-        std::bind(&PosePID::_subscriber_callback_target, this, std::placeholders::_1)
-    );
+        std::bind(&PosePID::_subscriber_callback_target, this, std::placeholders::_1));
+    // _subscription_target = this->create_subscription<geometry_msgs::msg::Pose2D>(
+    //     "target_pose",
+    //     _qos,
+    //     std::bind(&PosePID::_subscriber_callback_target, this, std::placeholders::_1));
     _subscription_selfpose = this->create_subscription<geometry_msgs::msg::PoseStamped>(
         "self_pose",
         _qos,
-        std::bind(&PosePID::_subscriber_callback_selfpose, this, std::placeholders::_1)
-    );
+        std::bind(&PosePID::_subscriber_callback_selfpose, this, std::placeholders::_1));
 
     publisher_vel = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", _qos);
 
@@ -56,24 +56,29 @@ allowed_area(get_parameter("allowed_area").as_double_array())
 
     _pub_timer = this->create_wall_timer(
         std::chrono::milliseconds(interval_ms),
-        [this] { if(is_autonomous)_publisher_callback(); }
-    );
+        [this]
+        { if(is_autonomous)_publisher_callback(); });
+  }
 
-}
-
-void PosePID::_publisher_callback(){
+  void PosePID::_publisher_callback()
+  {
     auto msg_vel = std::make_shared<geometry_msgs::msg::Twist>();
 
-    if(mode == Mode::stop){
-        msg_vel->linear.x = 0.0;
-        msg_vel->linear.y = 0.0;
-        msg_vel->angular.z = 0.0;
-        publisher_vel->publish(*msg_vel);
-        return;
-    }else mode = Mode::run;
+    if (mode == Mode::stop)
+    {
+      msg_vel->linear.x = 0.0;
+      msg_vel->linear.y = 0.0;
+      msg_vel->angular.z = 0.0;
+      publisher_vel->publish(*msg_vel);
+      return;
+    }
+    else
+      mode = Mode::run;
 
-    if(target == nullptr) return;
-    if(self_pose == nullptr) return;
+    if (target == nullptr)
+      return;
+    if (self_pose == nullptr)
+      return;
 
     const double current_yaw = std::atan2(
         2.0 * (self_pose->pose.orientation.w * self_pose->pose.orientation.z),
@@ -81,51 +86,56 @@ void PosePID::_publisher_callback(){
 
     RCLCPP_INFO(this->get_logger(), "current  x:%f  y:%f  yaw:%f", self_pose->pose.position.x, self_pose->pose.position.y, current_yaw);
 
-    msg_vel->linear.x = pid_linear_x.cycle(self_pose->pose.position.x, target->x)*linear_max_vel;
-    msg_vel->linear.y = pid_linear_y.cycle(self_pose->pose.position.y, target->y)*linear_max_vel;
-    msg_vel->angular.z = pid_angular.cycle(current_yaw, target->theta)*angular_max_vel;
+    msg_vel->linear.x = pid_linear_x.cycle(self_pose->pose.position.x, target->point.x) * linear_max_vel;
+    msg_vel->linear.y = pid_linear_y.cycle(self_pose->pose.position.y, target->point.y) * linear_max_vel;
+    msg_vel->angular.z = pid_angular.cycle(current_yaw, target->point.z) * angular_max_vel;
 
     // 出版
     publisher_vel->publish(*msg_vel);
-}
+  }
 
-void PosePID::_subscriber_callback_target(const geometry_msgs::msg::Pose2D::SharedPtr msg){
+  void PosePID::_subscriber_callback_target(const geometry_msgs::msg::PointStamped::SharedPtr msg)
+  {
     reset();
-    std::shared_ptr<geometry_msgs::msg::Pose2D> target_ = msg;
-    target_->x = constrain(target_->x, allowed_area.at(0), allowed_area.at(1));
-    target_->y = constrain(target_->y, allowed_area.at(2), allowed_area.at(3));
-    target_->theta = constrain(target_->theta, -pi<double>(), pi<double>());
+    std::shared_ptr<geometry_msgs::msg::PointStamped> target_ = msg;
+    target_->point.x = constrain(target_->point.x, allowed_area.at(0), allowed_area.at(1));
+    target_->point.y = constrain(target_->point.y, allowed_area.at(2), allowed_area.at(3));
+    target_->point.z = constrain(target_->point.z, -pi<double>(), pi<double>());
     this->target = target_;
-    RCLCPP_INFO(this->get_logger(), "目標姿勢入力  x:%f  y:%f  theta:%f", target->x, target->y, target->theta);
-}
-void PosePID::_subscriber_callback_selfpose(const geometry_msgs::msg::PoseStamped::SharedPtr msg){
+    RCLCPP_INFO(this->get_logger(), "目標姿勢入力  x:%f  y:%f  theta:%f", target->point.x, target->point.y, target->point.z);
+  }
+  void PosePID::_subscriber_callback_selfpose(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+  {
     self_pose = msg;
-}
-void PosePID::reset(){
+  }
+  void PosePID::reset()
+  {
     target = nullptr;
     pid_linear_x.reset();
     pid_linear_y.reset();
     pid_angular.reset();
-}
+  }
 
-
-/*基幹入力*/
-void PosePID::_subscriber_callback_stop(const std_msgs::msg::Empty::SharedPtr msg){
+  /*基幹入力*/
+  void PosePID::_subscriber_callback_stop(const std_msgs::msg::Empty::SharedPtr msg)
+  {
     mode = Mode::stop;
 
     RCLCPP_INFO(this->get_logger(), "停止");
-}
-void PosePID::_subscriber_callback_restart(const std_msgs::msg::Empty::SharedPtr msg){
+  }
+  void PosePID::_subscriber_callback_restart(const std_msgs::msg::Empty::SharedPtr msg)
+  {
     mode = Mode::stay;
     reset();
 
     RCLCPP_INFO(this->get_logger(), "再稼働");
-}
-void PosePID::_subscriber_callback_autonomous(const std_msgs::msg::Bool::SharedPtr msg){
+  }
+  void PosePID::_subscriber_callback_autonomous(const std_msgs::msg::Bool::SharedPtr msg)
+  {
     is_autonomous = msg->data;
     reset();
 
     RCLCPP_INFO(this->get_logger(), "自動：%d", is_autonomous);
-}
+  }
 
-}  // namespace pose_pid
+} // namespace pose_pid
